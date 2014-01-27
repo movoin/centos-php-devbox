92b40a95a2f8832925366ace8c33baae3293a61e
Allen <movoin@gmail.com>
2014-01-27 17:47:07 +0800

    update

--- puppet/modules/mysql/manifests/config.pp
+++ puppet/modules/mysql/manifests/config.pp
@@ -20,7 +20,7 @@ class mysql::config {
 
     exec { "install mysql db":
         cwd     => "${mysql::params::install_path}",
-        command => "scripts/mysql_install_db --user=mysql",
+        command => "${mysql::params::install_path}/scripts/mysql_install_db --user=mysql",
         unless  => "test -e /etc/init.d/mysqld",
         require => Exec["build mysql"]
     }
